ccffc5290d57b2b5e4b5b360d4c961e5c637ebd1
Daniil Nek <den11100@yandex.ru>
2019-02-06 09:11:51 +0300

    first-commit

---
+++ config/db.php
@@ -0,0 +1,23 @@
+<?php
+
+$localConfigPath = __DIR__ . DIRECTORY_SEPARATOR . 'db.local.php';
+
+$localConfig = [];
+if (file_exists($localConfigPath)) $localConfig = require $localConfigPath;
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=cmc2test',
+    'username' => 'root',
+    'password' => '111111',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
+
+return \yii\helpers\ArrayHelper::merge($config, $localConfig);
